4edccd8b6b464049600f660cab543d2946444ad6
Gokul Sudheesh <gokulsudheesh1@gmail.com>
2021-04-22 08:04:16 +0400

    small change in READ_STATEMENT's position :D

--- Week8/test1.c
+++ Week8/test1.c
@@ -3,8 +3,10 @@
 int main()
 {
     int a = 2,s,c;
-    char lol;
+    char lol, lol2[20];
     a = c%s;
     printf("Before: %d\n", a);
+    scanf("%s", lol2);
+    scanf("%d", a+s+c);
     printf("After: %d", a);//Type mismatch in arguments.
 }
