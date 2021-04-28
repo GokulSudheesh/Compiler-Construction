5f28d157c8f456f412f843d64ceabced2c06381f
Gokul Sudheesh <gokulsudheesh1@gmail.com>
2021-04-28 18:33:30 +0400

    added input files

--- CCAss/testlol.c
+++ CCAss/testlol.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int@funcname1():
+int@funcname1(para1::int,para2::float,para3::char,para4[1][2][3]::int):
     @kya::int
     printf("Hi")
     @lol::int
@@ -9,17 +9,27 @@ int@funcname1():
         @loop1::int 
     return lol
 
-void@funcname2():
+void@funcname2(aaaaah[10]::int):
     @kya2::int
     printf("Hi %d", kya2)
     @loll2::int
     @loll3::float
+int@*funcname2(aaaaah[10]::int):
+    @kya3::int
+    printf("Hi %d", kya3)
+    @*loll4::int
+    @loll5::float
+    return loll4
 int @ main():
-    @a,lmmao,Iwannadie=1::int
-    @b::int
-    @array1[20][30][40],a2,**p::int
-    @**c::int
-    @array2[20][30][40],a3::char
+    @a,lmmao,Iwannadie=1,arr[10]::int
+    @chh::char
+    @b::float
+    @array1[20][30][40],a2,***p::int
+    array1=p
+    @a3=funcname1(a,b,chh,array1)::int
+    funcname2(arr)
+    @**c::int//hi
+    @array2[20][30][40],a4::char
     for(@hmm=1::int,hmm<2,hmm++):
         @d::int
         @**k::char
