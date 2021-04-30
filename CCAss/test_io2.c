c2bc807f8327506f65514a20484431078d7483ac
Gokul Sudheesh <gokulsudheesh1@gmail.com>
2021-04-30 23:56:05 +0400

    updated inout files

--- CCAss/test_io2.c
+++ CCAss/test_io2.c
@@ -1,8 +1,10 @@
 import stdio.h
-
+import string.h
 int @ main():
     set a=2,b=3,array[20]::int
-    set str[30],name="Gokul",charr='!'::char
+    set str[30],name[30],charr::char
+    name="Gokul"
+    charr='!'
     read("%s",str)
     println("Hello %s%c",name,charr)
     println()
