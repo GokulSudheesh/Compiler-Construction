7f30d1056ee721ae8c948837ead360a88d276aa6
Gokul Sudheesh <38152409+GokulSudheesh@users.noreply.github.com>
2021-05-19 09:28:34 +0400

    Delete CC_Lab_discussion directory

--- CC_Lab_discussion/version_0/Lab-5.1-version2/input1-success.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int var1,var2,var3;
-	char var4,var5,var6;
-}
--- CC_Lab_discussion/version_0/Lab-5.1-version3/input1-success.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int var1,var2,var3;
-	char var4,var5,var6;
-
-	var1=var2+var3;
-	var4=var4*(var5+var6);
-}
